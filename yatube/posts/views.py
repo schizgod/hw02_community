0cfbec0982cdcfd38a04119f9f28584be32c641a
Vladimir Taras <schizgod@gmail.com>
2022-10-10 15:14:18 +0300

    trying..

---
+++ yatube/posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'index.html', context)
+
+
+def group_posts(request, slug):
+
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'group_list.html', context)
