5c508a2baba07f5fc4ff9681e7b7b34fa3b527e2
unknown <schizgod@gmail.com>
2022-10-24 17:57:20 +0300

    views index added paginator, 10 pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Group, Post
+from django.core.paginator import Paginator
 
 
 split_n = 10
 
 
 def index(request):
-    posts = Post.objects.select_related().all()[:split_n]
-    template = 'posts/index.html'
+    post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядеть так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10)
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
