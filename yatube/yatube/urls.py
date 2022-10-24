03edc6b57680caf73501c23529f8371e630a28c8
unknown <schizgod@gmail.com>
2022-10-24 08:53:14 +0300

    C:/Program Files/Git/auth path added

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
